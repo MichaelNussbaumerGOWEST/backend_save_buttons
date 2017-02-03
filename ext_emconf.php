cebef6faeef337f34017a41b91a9b98ab72d833a
MichaelNussbaumerGOWEST <m.nussbaumer@go-west.at>
2017-02-03 10:01:11 +0100

    Added version number and update TYPO3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
+			'typo3' => '7.6.0-8.5.99',
 		),
 		'conflicts' => array(
 		),
